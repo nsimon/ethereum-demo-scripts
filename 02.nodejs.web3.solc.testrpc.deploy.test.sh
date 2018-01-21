507301861ddb2f01fb877ece448eb9bd55ed3f13
Neil Simon <nsimon@solidcode.com>
2018-01-21 15:13:29 -0700

    Added ; to all js line endings

--- 02.nodejs.web3.solc.testrpc.deploy.test.sh
+++ 02.nodejs.web3.solc.testrpc.deploy.test.sh
@@ -150,153 +150,153 @@ cat << EOF > announcement.node.commands.js
 #!/usr/bin/env node
 
 // Load node_module: web3
-Web3 = require ("web3")
-console.log ("===================================")
-console.log ("Web3:")
-console.log ("===================================")
-console.log (Web3)
-console.log ()
+Web3 = require ("web3");
+console.log ("===================================");
+console.log ("Web3:");
+console.log ("===================================");
+console.log (Web3);
+console.log ();
 
 // Load node_module: solc
-solc = require ("solc")
-console.log ("===================================")
-console.log ("solc:")
-console.log ("===================================")
-console.log (solc)
-console.log ()
+solc = require ("solc");
+console.log ("===================================");
+console.log ("solc:");
+console.log ("===================================");
+console.log (solc);
+console.log ();
 
 // Load (core) node_module: fs
-fs = require ("fs")
-console.log ("===================================")
-console.log ("fs:")
-console.log ("===================================")
-console.log (fs)
-console.log ()
+fs = require ("fs");
+console.log ("===================================");
+console.log ("fs:");
+console.log ("===================================");
+console.log (fs);
+console.log ();
 
 // Load node_module: system-sleep
-sleep = require ("system-sleep")
-console.log ("===================================")
-console.log ("sleep:")
-console.log ("===================================")
-console.log (sleep)
-console.log ()
+sleep = require ("system-sleep");
+console.log ("===================================");
+console.log ("sleep:");
+console.log ("===================================");
+console.log (sleep);
+console.log ();
 
 // Create a new instance of the Web3 object
-web3 = new Web3 (new Web3.providers.HttpProvider ("http://localhost:8545"))
-console.log ("===================================")
-console.log ("web3:")
-console.log ("===================================")
-console.log (web3)
-console.log ()
+web3 = new Web3 (new Web3.providers.HttpProvider ("http://localhost:8545"));
+console.log ("===================================");
+console.log ("web3:");
+console.log ("===================================");
+console.log (web3);
+console.log ();
 
 // Test the web3 object, by displaying the 10 testrpc accounts
-console.log ("===================================")
-console.log ("accounts:")
-console.log ("===================================")
-accounts = web3.eth.accounts
-console.log ()
-console.log (accounts)
-console.log ()
+console.log ("===================================");
+console.log ("accounts:");
+console.log ("===================================");
+accounts = web3.eth.accounts;
+console.log ();
+console.log (accounts);
+console.log ();
 
 // Load the smart contact source
-sourceCode = fs.readFileSync ("announcement.sol").toString()
-console.log ("===================================")
-console.log ("sourceCode:")
-console.log ("===================================")
-console.log (sourceCode)
+sourceCode = fs.readFileSync ("announcement.sol").toString();
+console.log ("===================================");
+console.log ("sourceCode:");
+console.log ("===================================");
+console.log (sourceCode);
 
 // Compile the smart contract source - result is a json object with many fields
-compiledCode = solc.compile (sourceCode)
-console.log ("===================================")
-console.log ("compiledCode:")
-console.log ("===================================")
-console.log (compiledCode)
-console.log ()
+compiledCode = solc.compile (sourceCode);
+console.log ("===================================");
+console.log ("compiledCode:");
+console.log ("===================================");
+console.log (compiledCode);
+console.log ();
 
 // Extract the smart contract "application binary interface" (it's api)
-contractABI = JSON.parse (compiledCode.contracts [":announcement"].interface)
-console.log ("===================================")
-console.log ("contractABI:")
-console.log ("===================================")
-console.log (contractABI)
-console.log ()
+contractABI = JSON.parse (compiledCode.contracts [":announcement"].interface);
+console.log ("===================================");
+console.log ("contractABI:");
+console.log ("===================================");
+console.log (contractABI);
+console.log ();
 
 // Extract the smart contact byteCode - used to deploy the smart contract
-byteCode = compiledCode.contracts [":announcement"].bytecode
-console.log ("===================================")
-console.log ("byteCode:")
-console.log ("===================================")
-console.log (byteCode)
-console.log ()
+byteCode = compiledCode.contracts [":announcement"].bytecode;
+console.log ("===================================");
+console.log ("byteCode:");
+console.log ("===================================");
+console.log (byteCode);
+console.log ();
 
 // Instantiate the smart contract object
-announcementContract = web3.eth.contract (contractABI)
-console.log ("===================================")
-console.log ("announcementContract:")
-console.log ("===================================")
-console.log (announcementContract)
-console.log ()
+announcementContract = web3.eth.contract (contractABI);
+console.log ("===================================");
+console.log ("announcementContract:");
+console.log ("===================================");
+console.log (announcementContract);
+console.log ();
 
 // Deploy the smart contract to the testrpc blockchain
-console.log ("===================================")
-console.log ("announcementDeployed:")
-console.log ("===================================")
-announcementDeployed = announcementContract.new ({data: byteCode, from: web3.eth.accounts [0], gas: 4700000})
-console.log (announcementDeployed)
-console.log ()
+console.log ("===================================");
+console.log ("announcementDeployed:");
+console.log ("===================================");
+announcementDeployed = announcementContract.new ({data: byteCode, from: web3.eth.accounts [0], gas: 4700000});
+console.log (announcementDeployed);
+console.log ();
 
 // Allow the smart contract to fully deploy before continuing
-sleep (3000)
-console.log ()
+sleep (3000);
+console.log ();
 
 // Display the address of the deployed contract
-console.log ("===================================")
-console.log ("announcementDeployed.address:")
-console.log ("===================================")
-announcementDeployed.address
-console.log (announcementDeployed.address)
-console.log ()
+console.log ("===================================");
+console.log ("announcementDeployed.address:");
+console.log ("===================================");
+announcementDeployed.address;
+console.log (announcementDeployed.address);
+console.log ();
 
 // Get an instance of the deployed contract
-console.log ("===================================")
-console.log ("announcementInstance:")
-console.log ("===================================")
-announcementInstance = announcementContract.at (announcementDeployed.address)
-console.log (announcementInstance)
-console.log ()
+console.log ("===================================");
+console.log ("announcementInstance:");
+console.log ("===================================");
+announcementInstance = announcementContract.at (announcementDeployed.address);
+console.log (announcementInstance);
+console.log ();
 
 // Retrieve the original message (and see that the contructor has run)
 // no transaction -- only getting the state
-console.log ("===================================")
-console.log ("getAnnouncement ():")
-console.log ("===================================")
-message = announcementInstance.getAnnouncement()
-console.log ()
-console.log ("************************************************************************************")
-console.log ("* " + "message: " + message)
-console.log ("************************************************************************************")
-console.log ()
+console.log ("===================================");
+console.log ("getAnnouncement ():");
+console.log ("===================================");
+message = announcementInstance.getAnnouncement();
+console.log ();
+console.log ("************************************************************************************");
+console.log ("* " + "message: " + message);
+console.log ("************************************************************************************");
+console.log ();
 
 // Update the message
-newAnnouncement = "Goodbye from the automated test: $SCRIPTNAME"
+newAnnouncement = "Goodbye from the automated test: $SCRIPTNAME";
 fromAccount     = web3.eth.accounts [0];
-console.log ("===================================")
-console.log ("setAnnouncement ('" + newAnnouncement + "', {from: " + fromAccount + "}):")
-console.log ("===================================")
-tx = announcementInstance.setAnnouncement (newAnnouncement, {from: fromAccount})
-console.log ("tx: " + tx)
-console.log ()
+console.log ("===================================");
+console.log ("setAnnouncement ('" + newAnnouncement + "', {from: " + fromAccount + "}):");
+console.log ("===================================");
+tx = announcementInstance.setAnnouncement (newAnnouncement, {from: fromAccount});
+console.log ("tx: " + tx);
+console.log ();
 
 // Retrieve the updated message
 // no transaction -- only getting the state
-console.log ("===================================")
-console.log ("getAnnouncement ():")
-console.log ("===================================")
-message = announcementInstance.getAnnouncement()
-console.log ()
-console.log ("**************************************************************************************")
-console.log ("* message: " + message)
-console.log ("**************************************************************************************")
+console.log ("===================================");
+console.log ("getAnnouncement ():");
+console.log ("===================================");
+message = announcementInstance.getAnnouncement();
+console.log ();
+console.log ("**************************************************************************************");
+console.log ("* message: " + message);
+console.log ("**************************************************************************************");
 
 EOF
 
